3363622e46575ffe7f27c2185f92c047d77e7ab6
Andrey Gulenko <consul-guard@proton.me>
2024-02-01 22:47:59 +0300

    Создали свою точку входа в приложение (класс Program метод Main)

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTestSignalR_1
+{
+    public static class Program
+    {
+        [STAThread] //необходим обязательный атрибут 
+        public static void Main() // метод - точка входа в приложение (можно скопировать из файла .../obj/Debug/.../App.g.cs - 64я строчка
+        {
+            App app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+        //теперь мы контролируем факт запуска приложения в его самой начальной точке
+    }
+}
