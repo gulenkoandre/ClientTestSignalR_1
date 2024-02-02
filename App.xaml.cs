758ed8f3160b560e5bf60d30a4436ade84b7ec5b
Andrey Gulenko <consul-guard@proton.me>
2024-02-02 08:30:40 +0300

    Все подготовлено для работы с сервисами

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Configuration;
+﻿// Ignore Spelling: App
+
+using ClientTestSignalR_1.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +14,51 @@ namespace ClientTestSignalR_1
     /// </summary>
     public partial class App : Application
     {
+        //создаем Хост
+        private static IHost? _host;
+        public static IHost Host => _host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build(); //при первом обращении Хост будет создан, будут сконфигурированы все его сервисы и  ими можно пользоваться
+                                                                                                                   //теперь через статическое поле Host класса App мы всегда можем обратиться к хосту
+
+
+        //при запуске приложения, мы запускаем и Хост
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            IHost host = Host;
+
+            base.OnStartup(e);
+
+            await host.StartAsync().ConfigureAwait(false);
+        }
+
+        // при выходе из приложения, мы закрываем и Хост
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            IHost host = Host;
+
+            base.OnExit(e);
+
+            await host.StopAsync().ConfigureAwait(false);
+
+            host.Dispose();
+
+            _host = null;
+        }
+
+        /// <summary>
+        /// метод конфигурации сервисов (забирается в классе Program)
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="services"></param>
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            //здесь добавляем все сервисы, которые нам потребуются для дальнейшей работы
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<VM>();
+
+            //AddTransient() создает transient-объекты. Такие объекты создаются при каждом обращении к ним.
+            //AddScoped() создает один экземпляр объекта для всего запроса.
+            //AddSingleton() создает один объект для всех последующих запросов, при этом объект создается только тогда, когда он непосредственно необходим.
+        }
     }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,10 @@ using System.Threading.Tasks;
 
 namespace ClientTestSignalR_1
 {
-    public static class Program
+    /// <summary>
+    /// класс для создания своей точки входа в приложения, организации Hosting, организации Dependency Injection
+    /// </summary>
+    public static class Program 
     {
         [STAThread] //необходим обязательный атрибут 
         public static void Main() // метод - точка входа в приложение (можно скопировать из файла .../obj/Debug/.../App.g.cs - 64я строчка
@@ -16,5 +21,37 @@ namespace ClientTestSignalR_1
             app.Run();
         }
         //теперь мы контролируем факт запуска приложения в его самой начальной точке
+
+        //метод в котором мы будем создавать Host. Обязательно: он должен быть публичным; должен возвращать интерфейс IHostBuilder;
+        //должен иметь имя CreateHostBuilder 
+        public static IHostBuilder CreateHostBuilder(string[] Args)
+        {
+            IHostBuilder hostBuilder = Host.CreateDefaultBuilder(Args);
+
+            //настраиваем ряд конфигурационных возможностей
+            hostBuilder.UseContentRoot(Environment.CurrentDirectory); //рабочий каталог нашего приложения
+
+            //добавляем конфигурацию нашего приложения (host - ссылка на host, cfg - ссылка на объект конфигурации
+            hostBuilder.ConfigureAppConfiguration((host, cfg) => 
+            {
+                //добавляем базовый путь
+                cfg.SetBasePath(Environment.CurrentDirectory);
+
+                // добавляем файл конфигурации с выбранным именем,
+                // optional:true - означает, что если файла нет, то ошибки не будет
+                // reloadOnChange:true - означает, что файл надо перечитать, если система обнаруживает, что он изменился
+                cfg.AddJsonFile("appsettings.json", optional: true, reloadOnChange:true);
+
+            });
+
+            //у hostBuilder вызываем метод ConfigureServices, куда передаем свой метод конфигурации сервисов
+            hostBuilder.ConfigureServices(App.ConfigureServices);
+            //свой метод конфигурации сервисов, который мы тоже назвали ConfigureServises опишем в классе App
+
+            //сами сервисы опишем внутри нашего приложения (App)
+
+
+            return hostBuilder;
+        }
     }
 }
